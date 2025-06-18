0eddb66798f159ebd01b83c29e8daecd04c9d747
Tuubby <mmantas.kazlauskas@gmail.com>
2025-06-18 11:29:59 +0300

    Init Commit

---
+++ Main.lua
@@ -0,0 +1,9 @@
+---@class ChattyLittleNpc_Shadowlands_voiceovers
+local ChattyLittleNpc_Shadowlands_voiceovers = LibStub("AceAddon-3.0"):NewAddon("ChattyLittleNpc_Shadowlands_voiceovers")
+
+ChattyLittleNpc_Shadowlands_voiceovers.Metadata = {
+    ContainsQuestVoiceovers = true,
+    ContainsGossipVoiceovers = false,
+    ContainsGameObjectVoiceovers = false,
+    ContainsItemVoiceovers = false
+}
